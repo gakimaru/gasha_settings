db429826fff540014f1f8cdaddcd6c8181d3b1b4
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-06-24 19:56:13 +0900

    ・全てのマクロ名を調整。（必ず GASHA_ で始まる）

--- include/gasha/build_settings/project/first_settings.h
+++ include/gasha/build_settings/project/first_settings.h
@@ -17,33 +17,31 @@
 //※プロジェクトの都合により、ライブラリのネームスペースを変更もしくは削除したい場合は、
 //　このマクロを書き換えて使用する
 
-#define USE_NAME_SPACE_GASHA//GASHA用のネームスペースを使用する場合、このマクロを有効化する
+#define GASHA_USE_NAME_SPACE//GASHA用のネームスペースを使用する場合、このマクロを有効化する
 
-#ifdef USE_NAME_SPACE_GASHA//以下、ネームスペースを使用する場合
+#ifdef GASHA_USE_NAME_SPACE//以下、ネームスペースを使用する場合
 
-	#define NAMESPACE_NAME_GASHA gasha//ネームスペース名　※ネームスペースを変更したい場合、このマクロを書き換える
+	#define GASHA_NAMESPACE_NAME gasha//ネームスペース名　※ネームスペースを変更したい場合、このマクロを書き換える
 
-	#define NAMESPACE_GASHA_BEGIN namespace NAMESPACE_NAME_GASHA {//ネームスペース開始宣言
-	#define NAMESPACE_GASHA_END }//ネームスペース終了宣言
+	#define GASHA_NAMESPACE_BEGIN namespace GASHA_NAMESPACE_NAME {//ネームスペース開始宣言
+	#define GASHA_NAMESPACE_END }//ネームスペース終了宣言
 
-	#define USING_NAMESPACE_GASHA using namespace NAMESPACE_NAME_GASHA//ネームスペース使用宣言
+	#define GASHA_USING_NAMESPACE using namespace GASHA_NAMESPACE_NAME//ネームスペース使用宣言
 	
-	#define GASHA_ NAMESPACE_NAME_GASHA:://ネームスペース指定
+	#define GASHA_ GASHA_NAMESPACE_NAME:://ネームスペース指定
 
-#else//USE_NAME_SPACE_GASHA//以下、ネームスペースを使用しない場合の設定
+#else//GASHA_USE_NAME_SPACE//以下、ネームスペースを使用しない場合の設定
 
-	#define NAMESPACE_NAME_GASHA//ネームスペース名
+	#define GASHA_NAMESPACE_NAME//ネームスペース名
 
-	#define NAMESPACE_GASHA_BEGIN//ネームスペース開始宣言
-	#define NAMESPACE_GASHA_END//ネームスペース終了宣言
+	#define GASHA_NAMESPACE_BEGIN//ネームスペース開始宣言
+	#define GASHA_NAMESPACE_END//ネームスペース終了宣言
 
-	#define USING_NAMESPACE_GASHA;//ネームスペース使用宣言
+	#define GASHA_USING_NAMESPACE;//ネームスペース使用宣言
 
 	#define GASHA_//ネームスペース指定
 
-#endif//USE_NAME_SPACE_GASHA
-
-#define INCLUDE_GASHA(file) <gasha/file> //インクルード
+#endif//GASHA_USE_NAME_SPACE
 
 #endif//__PROJECT_BUILD_SETTINGS_FIRST_H_
 
--- include/gasha/build_settings/project/last_settings.h
+++ include/gasha/build_settings/project/last_settings.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#ifndef __PROJECT_BUILD_SETTINGS_H_
-#define __PROJECT_BUILD_SETTINGS_H_
+#ifndef __PROJECT_BUILD_SETTINGS_LAST_H_
+#define __PROJECT_BUILD_SETTINGS_LAST_H_
 
 //--------------------------------------------------------------------------------
 // project_build_settings.h
@@ -17,54 +17,54 @@
 
 //--------------------------------------------------------------------------------
 //SSE命令設定
-#ifdef IS_X86//x86,x64系CPUの場合のみ設定可
+#ifdef GASHA_IS_X86//x86,x64系CPUの場合のみ設定可
 //※使用するバージョン以前のマクロは全て有効化すること
-#define USE_SSE//SSE命令を使用
-#define USE_SSE2//SSE2命令を使用
-#define USE_SSE3//SSE3命令を使用
-//#define USE_SSE4A//SSE4a命令(AMD)を使用
-#define USE_SSE4_1//SSE4.1命令を使用
-#define USE_SSE4_2//SSE4.2命令を使用
-#define USE_POPCNT//POPCNT命令を使用
-#define USE_AES//AES命令を使用
-#define USE_AVX//AVX命令を使用
-//#define USE_AVX2//AVX2命令を使用
-#endif//IS_X86
+#define GASHA_USE_SSE//SSE命令を使用
+#define GASHA_USE_SSE2//SSE2命令を使用
+#define GASHA_USE_SSE3//SSE3命令を使用
+//#define GASHA_USE_SSE4A//SSE4a命令(AMD)を使用
+#define GASHA_USE_SSE4_1//SSE4.1命令を使用
+#define GASHA_USE_SSE4_2//SSE4.2命令を使用
+#define GASHA_USE_POPCNT//POPCNT命令を使用
+#define GASHA_USE_AES//AES命令を使用
+#define GASHA_USE_AVX//AVX命令を使用
+//#define GASHA_USE_AVX2//AVX2命令を使用
+#endif//GASHA_IS_X86
 
 //--------------------------------------------------------------------------------
 //CRC32設定
-#define CRC32_IS_CRC32C//CRC32計算（多項式）を、CRC-32C(Castagnoli)にする場合、このマクロを有効化する
-                       //※無効化時は、標準的なIEEE802.3のCRC-32を使用する。
-                       //※CRC-32Cの方が衝突の機会が少なく優れている。
-                       //※CRC-32C有効化＋SSE4.2使用で計算が高速化される。
+#define GASHA_CRC32_IS_CRC32C//CRC32計算（多項式）を、CRC-32C(Castagnoli)にする場合、このマクロを有効化する
+                             //※無効化時は、標準的なIEEE802.3のCRC-32を使用する。
+                             //※CRC-32Cの方が衝突の機会が少なく優れている。
+                             //※CRC-32C有効化＋SSE4.2使用で計算が高速化される。
 
-#define CRC32_USE_STATIC_TABLE//CRC32計算に、事前計算済みの多項式テーブルを用いる場合、このマクロを有効化する
-                              //※4×256=1024バイトのメモリを要するが、高速に処理できる。
-                              //※CRC-32C有効化＋SSE4.2使用時は、このマクロの指定に関係なく、
-                              //　ランタイム時にはSSE命令を使用する（その方が高速）
+#define GASHA_CRC32_USE_STATIC_TABLE//CRC32計算に、事前計算済みの多項式テーブルを用いる場合、このマクロを有効化する
+                                    //※4×256=1024バイトのメモリを要するが、高速に処理できる。
+                                    //※CRC-32C有効化＋SSE4.2使用時は、このマクロの指定に関係なく、
+                                    //　ランタイム時にはSSE命令を使用する（その方が高速）
 
 //--------------------------------------------------------------------------------
 //値交換設定
-//#define SWAP_VALUES_USE_MEMCPY//swapValues/rotateValues関数でmemcpyを使用する場合、このマクロを有効化する（通常はムーブコンストラクタ／ムーブオペレータ使用）
+//#define GASHA_SWAP_VALUES_USE_MEMCPY//swapValues/rotateValues関数でmemcpyを使用する場合、このマクロを有効化する（通常はムーブコンストラクタ／ムーブオペレータ使用）
 
 //--------------------------------------------------------------------------------
 //ソート設定
 
-#define QUICK_SORT_NO_USE_RECURSIVE_CALL//クイックソートの再帰処理版を無効にする場合は、このマクロを有効にする
+#define GASHA_QUICK_SORT_NO_USE_RECURSIVE_CALL//クイックソートの再帰処理版を無効にする場合は、このマクロを有効にする
 
 #ifdef _OPENMP//OpenMP有効時
 
-	#define ODD_EVEN_SORT_USE_OPENMP//奇遇転置ソート：OpenMPを使用する場合は、このマクロを有効にする
+	#define GASHA_ODD_EVEN_SORT_USE_OPENMP//奇遇転置ソート：OpenMPを使用する場合は、このマクロを有効にする
 
-	#define SHEAR_SORT_USE_OPENMP//シェアソート：OpenMPを使用する場合は、このマクロを有効にする
-	//#define SHEAR_SORT_USE_OPENMP_NEST//シェアソート：OpenMPのparallelの入れ子処理を使用する場合は、このマクロを有効にする ※有効にするとかえって遅くなる、もしくは、正常に動作しない
+	#define GASHA_SHEAR_SORT_USE_OPENMP//シェアソート：OpenMPを使用する場合は、このマクロを有効にする
+	//#define GASHA_SHEAR_SORT_USE_OPENMP_NEST//シェアソート：OpenMPのparallelの入れ子処理を使用する場合は、このマクロを有効にする ※有効にするとかえって遅くなる、もしくは、正常に動作しない
 
-	//#define INPLACE_MERGE_SORT_USE_OPENMP//インプレースマージソート：OpenMPを使用する場合は、このマクロを有効にする ※有効にすると遅くなることがある
+	//#define GASHA_INPLACE_MERGE_SORT_USE_OPENMP//インプレースマージソート：OpenMPを使用する場合は、このマクロを有効にする ※有効にすると遅くなることがある
 
-	//#define QUICK_SORT_USE_OPENMP//クイックソート：OpenMPを使用する場合は、このマクロを有効にする ※有効にするとかえって遅くなる
+	//#define GASHA_QUICK_SORT_USE_OPENMP//クイックソート：OpenMPを使用する場合は、このマクロを有効にする ※有効にするとかえって遅くなる
 
 #endif//_OPENMP
 
-#endif//__PROJECT_BUILD_SETTINGS_H_
+#endif//__PROJECT_BUILD_SETTINGS_LAST_H_
 
 // End of file
